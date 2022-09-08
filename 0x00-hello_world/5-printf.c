13f9576ff7a5358098942af6a3b4d9552c23405b
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-08 07:08:33 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main -  is used to print stdout
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
