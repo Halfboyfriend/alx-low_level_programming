bb71fa7d856507ec598743af9cfee5e35e5369a9
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-08 06:40:40 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
+/*This declare the output and input of the file*/
 #include <stdio.h>
+/* if correct, return 0*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	result (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
