16789a0e09bbd0cb373ac5929fe2b9495a8e206d
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-09 18:28:09 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,8 @@ int main(void)
 	char lowerCase;
 	char fCase;
 
-	for (lowerCase = 'a'; lowerCase <= 'd'; lowerCase++; && fCase = 'f'; fCase <= 'p'; fCase++)
+	for (lowerCase = 'a'; lowerCase <= 'd'; lowerCase++)
+	for (fCase = 'f'; fCase <= 'p'; fCase++)
 	{
 	putchar(lowerCase);
 	}
