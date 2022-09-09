ebef0f11f904733c2c9f6abfabb6048ddf584620
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-09 09:57:07 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints out the stdout
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char lower;
+	char upper;
+
+	for (lower = 'a', lower <= 'z', lower++)
+	{
+	for (upper = 'A', upper <= 'Z', upper++)
+	{
+	putchar(upper);
+	}
+	putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
