cf4443562517f662ac1c4d57f235066caf9a52e9
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-09 16:28:10 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	int n;
 	int m;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
@@ -23,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-	print("Las digit of %d is %d and is 0\n", n, m);
+	printf("Last digit of %d is %d and is 0\n", n, m);
 	}
 	return (0);
 }
