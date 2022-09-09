1036cce799099e6ce75e8d7bf654853a18d563eb
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-09 23:56:33 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - prints the stdout
  * Return: 0 if successful
@@ -9,8 +11,8 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-	printf(i);
+	printf("%d", i);
 	}
-	printf('%d\n');
+	printf('\n');
 	return (0);
 }
