04dd85c67caf2a5bd82bb2e56e6fdab3c9f4a71a
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-09 09:26:18 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the stdo
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = a; alpha <= 'z'; alpha++)
+	{
+	putchar("%c/n", alpha);
+	{
+	return (0);
+}
