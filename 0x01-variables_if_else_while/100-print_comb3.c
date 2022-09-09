2f549fb384148fb60b2b80023a31db4a24223391
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-09 16:03:49 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.io>
+#include <time.h>
+/**
+ * main - prints the stdout
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '01'; a <= '10'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
