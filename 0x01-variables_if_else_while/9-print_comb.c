7a86b094d364288bd63ed97a0b3ca70e7abf682e
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-09 23:40:46 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 	int a;
-	int b;
 
 	for (a = '0'; a <= '9'; a++)
-	for (b = ','; b <= ','; b++)
 	{
 	putchar(a);
-	putchar(b);
+	if (a != '9')
+	putchar(' ');
+	putchar(',');
 	}
+	putchar('\n');
 	return (0);
 }
