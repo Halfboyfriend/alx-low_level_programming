b409fe7cd1c192d06a1915e338e25b18d7bbfbd5
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-09 14:31:18 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints the stdout
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; 1++)
+	{
+	putchar("%d", i);
+	}
+	putchar('\n');
+}
