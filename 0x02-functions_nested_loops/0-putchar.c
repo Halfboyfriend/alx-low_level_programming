0448c1fc875f5b2006ca7a50eebdad24de2a8fa5
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-13 02:23:01 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints the stdout
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	putchar('_putchar\n');
+	return (0);
+)
