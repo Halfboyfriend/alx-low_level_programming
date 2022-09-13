03f7eb701fc994574ce2b59232938cdca2982a14
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-13 07:31:07 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
-#include <main.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 /**
  * main - prints the stdout
  * Return: 0 if successful
  */
 int main(void)
 {
-	putchar('_putchar\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-)
+}
