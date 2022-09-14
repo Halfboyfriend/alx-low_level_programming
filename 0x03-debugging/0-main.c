de116e3064fdbd241f00d21d8881b6cfa063448b
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-14 13:03:58 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - test function that prints if interger is positive or negative
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 98;
+	if (i <= 98)
+	{
+	printf("%d is positive\n", i);
+	}
+	else
+	{
+	printf("%d is negative\n", i);
+	}
+}
