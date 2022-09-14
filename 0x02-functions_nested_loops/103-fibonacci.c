38b565469719ea5cd471d1df6813b25543aeece0
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-14 01:12:06 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints the fibonacci
+ * Return: 0 Always
+ */
+int main(void)
+{
+	long int i, a, b, result;
+
+	a = 1;
+	b = 2;
+	for (i = 1; i <= 89; ++i)
+	{
+	if (a != 4000000)
+	{
+	printf("%d, ", a);
+	}
+	else
+	{
+	printf("%d\n", a);
+	}
+	result = a + b;
+	a = b;
+	b = result;
+	}
+	return (0);
+}
