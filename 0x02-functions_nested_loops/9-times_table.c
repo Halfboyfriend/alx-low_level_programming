b9d1eea05625ff2648e036e7d286e6b7393e6b8a
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-13 20:10:18 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,31 +9,17 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-	for (b = 0; b <= 9; b++)
-	{
-	cal = a * b;
-	if (b <= 9)
-	{
-	if (b != 0)
-	{
-	_putchar(' ');
-	}
-	_putchar(cal + '0');
-	}
-	else
-	{
-	_putchar(cal / 10 + '0');
-	_putchar(cal % 10 + '0');
-	}
-	if (b != 9)
+	for (b = 1; b <= 9; b++)
 	{
 	_putchar(',');
 	_putchar(' ');
-	}
+	cal = a * b;
+	if (cal <= 9)
+	_putchar(' ');
 	else
-	{
+	_putchar((cal / 10) + '0');
+	_putchar((cal % 10) + '0');
+	}
 	_putchar('\n');
-	}
-	}
-	}
+
 }
