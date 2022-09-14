d8a1e539261ecdbfa14549c245a553e4a1312434
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-14 21:07:40 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,11 +3,12 @@
  * main - test function that prints if interger is positive or negative
  * Return: 0
  */
-void positive_or_negative(int i)
+int main(void)
 {
 	int i;
 
 	i = 98;
+	positive_or_negative(i);
 	if (i <= 98)
 	{
 	printf("%d is positive\n", i);
