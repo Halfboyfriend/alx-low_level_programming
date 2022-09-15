130b7c875a25f148352fbd50dbf0e6a0cd56b34d
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-15 23:06:54 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,25 +5,19 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
+	int b1, b2;
+	
+	for (b1 = 0; b1 < n; b1++)
+	{
+	for (b2 = 0; b2 < b1; b2++)
+	{
+	_putchar(' ');
+	}
+	_putchar(98);
+	_putchar('\n');
+	}
 	if (n <= 0)
 	{
 	_putchar('\n');
 	}
-	else
-	{
-	for (i = 1; i < n; i++)
-	{
-	if (i > 1)
-	{
-	for (j = 1; j <= i -1; j++)
-	{
-	_putchar(' ');
-	}
-	}
-	_putchar('\\');
-	_putchar('\n');
-	}
-	} 
 }
