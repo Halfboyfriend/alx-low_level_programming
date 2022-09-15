85c571d172cef064eee431c51b895f0e732dfa9f
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-16 00:16:56 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,17 +7,26 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
-	for (i = 1; i <= size; i++)
-	{
-	for (j = 1; j <= size; j++)
-	{
-	_putchar('#');
-	}
-	_putchar(' ');
-	}
 	if (size < 0)
 	{
 	_putchar('\n');
+	}
+	else
+	{
+	for (i = 1; i <= size; i++)
+	{
+	for (j = size - i; j >= 1; j--)
+	{
+	_putchar(' ');
+	}
+	for (k = 1; k <= i; k++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	
 	}
 }
