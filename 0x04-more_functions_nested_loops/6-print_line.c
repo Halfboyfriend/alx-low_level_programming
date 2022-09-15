bf0153be0a5c7ec5ee0bc4bf3ac442da89bb7427
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-15 14:19:36 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - function declsred
+ * @n: integer assigned
+ * Return: 0 if sucesssful
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+	_putchar('\n');
+	}
+	_putchar(' ');
+}
