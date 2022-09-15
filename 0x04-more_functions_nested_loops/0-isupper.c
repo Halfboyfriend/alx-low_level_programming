40bb1f19aae65dbc90b3e1837293dcb895135ad7
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-15 04:27:11 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - function to print uppercase
+ * @c: interger assigned
+ * Return: 1 if true else 0
+ */
+int _isupper(int c);
+{
+	if (isupper(c))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
