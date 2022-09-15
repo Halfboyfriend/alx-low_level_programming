99973e4e9888a7e4d143c3ed94fc6d5296dea316
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-15 19:46:22 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,16 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, ten, unit;
 
 	for (i = 0; i <= 10; i++)
 	{
 	for (j = 0; j <= 14; j++)
-	{
-	_putchar(j);
-	}
+	ten = j / 10;
+	unit = j % 10;
+	if (j > 9)
+	_putchar(tens + '0');
+	_putchar(unit + '0');
 	_putchar('\n');
 	}
 }
