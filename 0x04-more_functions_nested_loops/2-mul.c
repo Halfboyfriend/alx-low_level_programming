dc129b560818ebbe512cdf4dade4bfbe4d53f7ec
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-15 05:05:13 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - function
+ * @a: an int
+ * @b: an int
+ * Return: 0 always
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	_putchar(mul);
+	return (0);
+}
