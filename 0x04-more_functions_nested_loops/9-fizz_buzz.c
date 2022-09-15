264c5696e9d84a32e4c52fb53083044b49258456
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-16 00:00:31 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,12 +9,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-	if (((i % 3) || (i % 5)) == 0)
-	printf("Fizzbuzz ");
+	if (i % 15 == 0)
+	printf("FizzBuzz");
 	else if (i % 5 == 0)
-	printf("Buzz ");
+	printf("Buzz");
 	else if (i % 3 == 0)
-	printf("Fizz ");
+	printf("Fizz");
 	else
 	printf("%d ", i);
 	if (i != 100)
