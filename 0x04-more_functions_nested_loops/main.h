9b731b974823134f9d1cfd58b9f8fe5b4458b39e
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-15 12:35:12 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,4 +9,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 
-#endif /** MAIN_H **/
+#endif
