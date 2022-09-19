59d2b4f157c32cee615f27c2f4516ffe6a2ce4e1
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-19 23:43:43 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,17 @@ void puts_half(char *str)
 	int l, i;
 
 	for (l = 0; str[l] != '\0'; l++)
-	{
+	;
 	if (l % 2 == 0)
 	{
 		for (i = l / 2; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	}
-	_putchar('\n');
+	else
+	{
+	for (i = ((l - 1) / 2) + 1; str[i] != '\0'; i++)
+	_putchar(str[i]);
 	}
+	_putchar('\n');
+	
 }
