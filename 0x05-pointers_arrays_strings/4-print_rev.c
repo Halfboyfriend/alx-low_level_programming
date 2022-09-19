eda4848e169178ec7435441584bf14072c260c1e
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-19 15:38:53 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,20 @@
 #include "main.h"
-#include <string.h>
 /**
  * print_rev - function declared
  * @s: integer declared
  */
 void print_rev(char *s)
 {
-	for (int i =strlen(s)-1; i >= 0; i--)
+	int i = 0;
+	
+	while(s[i] != 0)
 	{
+	i++;
+	}
+	while (i > 0)
+	{
+	i--;
 	_putchar(s[i]);
 	}
+	_putchar('\n');
 }
