05a613ff36481fa41defb8525a35b8357404271f
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-19 22:19:39 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,11 @@ void rev_string(char *s)
 	int l, i;
 
 	l = strlen(s);
-	for (i = 0; i < l / '50'; i++)
+	for (i = 0; i < l / 2; i++)
 	{
 	c = s[i];
 	s[i] = s[l - 1 - i];
 	s[l - 1 - i] = c;
 	}
-	_putchar(c);
+	_putchar(s[i]);
 }
