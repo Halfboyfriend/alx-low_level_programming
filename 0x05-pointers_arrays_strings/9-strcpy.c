c2e46152781971837569561ea0628f061cc6fcf6
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-20 00:35:18 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,12 +2,12 @@
 /**
  * _strcpy - function to copy a string
  * @src: assinged int
- * @dest: assigned int 
+ * @dest: assigned int
  */
 char *_strcpy(char *dest, char *src)
 {
 	int abc = 0;
-	
+
 	while (*(src + abc) != '\0')
 	{
 	*(dest + abc) = *(src + abc);
