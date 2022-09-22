ca4c66c6524d8a25432c31d5723f8744adb8fd6f
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-22 14:37:50 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,15 +6,15 @@
  **/
 char *string_toupper(char *c)
 {
-	int i;
-
+	int i = 0;
+	
 	while (c[i] != '\0')
 	{
 		if (c[i] >= 'a' && c[i] <= 'z')
 		{
 			c[i] = c[i] - 32;
 		}
-	i++
+	i++;
 	}
 	return (c);
 }
