d3eb8e67cdda38ba5f141b19409de682656f0d2c
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-22 13:31:09 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - function that prints an integer
+ * @n: is an integer assigned
+ * Return: nothing
+ **/
+void print_number(int n)
+{
+	unsigned int i;
+	
+	if (n < 0 )
+	{
+		_putchar('-');
+		i = -n;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n / 10)
+	{
+		print_number(i / 10);
+	}
+	else 
+	{
+	_putchar((i % 10) + '0');
+	}
+}
