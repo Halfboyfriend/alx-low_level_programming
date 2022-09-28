41377aa1dded7439e1413f132bd6fcc51eea91ac
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-28 18:43:25 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,8 +6,12 @@
  **/
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	return;
-	puts(s);
-	_puts_recursion(*s + '1');
+	if (*s != '\0')
+	{
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+	}
+	else
+	_putchar('\n');
 }
