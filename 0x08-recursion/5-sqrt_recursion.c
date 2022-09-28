986f2d37f9e691f6cd4103bd2e2d583dd5585514
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-28 22:10:16 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * sqrt - funtion
+ * sqr - funtion
  * _sqrt_recursion - funtion that returns square root
  * @n: int variable
  * @cal: indirect recursive function
  * Return: values && -1i
  **/
-int sqrt(int n, int cal)
+int sqr(int n, int cal)
 {
 	if (cal * cal == n)
 	return (cal);
 	else if (cal * cal < n)
-	return (sqrt(n, cal + 1));
+	return (sqr(n, cal + 1));
 	else
 	return (-1);
 }
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (sqr(n, 1));
 }
