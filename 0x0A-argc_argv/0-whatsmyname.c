2676617627be1c14549562466b65967b83c25caa
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-09-30 13:21:29 -0700

    Task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - funcion prints the name
+ * @argc: arguments
+ * @argv: pointer string
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	prinf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function that prints number of arg
+ * @argc: arguments counts
+ * @argv: pointer string vecore
+ * Return: o
+ **/
+int main(int argc, char *argv[])
+{
+	prinf("argc = %d\n", argc)
+	return (0);
+}
