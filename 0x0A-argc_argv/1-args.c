a609a6f455f1c47307e9cdc2ad611186b818b6b2
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-09-30 21:28:06 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - function that prints number of arg
  * @argc: arguments counts
  * @argv: pointer string vecore
- * Return: o
- **/
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
 	(void)argv;
 
-	prinf("argc = %d\n", argc - 1);
+	prinf("%d\n", argc - 1);
 	return (0);
 }
