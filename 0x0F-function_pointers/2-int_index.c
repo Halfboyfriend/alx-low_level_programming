ce92515c4ee50034241cecb2e857d186060e3edd
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-10-12 19:58:52 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,6 @@
 /*
  * File: 2-int_index.c
- * Auth: Brennan D Baraban
+ * Auth: Owoyemi Idris
  */
 
 #include "function_pointers.h"
@@ -12,20 +12,18 @@
  * @cmp: A pointer to the function to be used to compare values.
  *
  * Return: If no element matches or size <= 0 - -1.
- *         Otherwise - The index of the first element for which
- *                     the cmp function does not return 0.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index;
+	int i;
 
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
-	for (index = 0; index < size; index++)
+	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[index]) != 0)
-			return (index);
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
 
 	return (-1);
