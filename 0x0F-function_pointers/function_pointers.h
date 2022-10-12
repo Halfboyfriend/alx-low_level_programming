fa873309118138478bcb7615cc09125ffe489bf7
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-10-12 19:29:33 +0100

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,13 +2,13 @@
 #define FUNCTION_POINTERS_H
 
 /**
- * File: function_pointer.h
+ * File: function_pointers.h
  * Desc: Headerfile containing all prototype for all fuctions
+ * used in the 0x0E-function_pointers.
  */
 
 #include <stdlib.h>
 
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
