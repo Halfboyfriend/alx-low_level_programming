e3e619e645b967b103dc2c9f301f47e7299d0baa
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-10-13 17:55:03 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -67,7 +67,7 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0, j = 0;
 	char *separator = "";
-
+	
 	printer_t funcs[] = {
 		{"c", print_char},
 		{"i", print_int},
@@ -77,20 +77,23 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 
-	while (format && (*(format + 1)))
+	while (format && (*(format + i)))
 	{
 		j = 0;
 
 		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
 			j++;
+
 		if (j < 4)
 		{
 			printf("%s", separator);
 			funcs[j].print(args);
 			separator = ", ";
 		}
+
 		i++;
 	}
+
 	printf("\n");
 
 	va_end(args);
