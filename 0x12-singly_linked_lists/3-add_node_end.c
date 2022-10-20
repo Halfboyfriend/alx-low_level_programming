6a243f0af6a6ba438d208c739f211d6eec26ac72
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-10-20 23:16:07 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,20 +17,24 @@ list_t *add_node_end(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	dup == strdup(str);
+
+	dup = strdup(str);
 	if (str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
+
 	for (len = 0; str[len];)
 		len++;
+
 	new->str = dup;
 	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
 		*head = new;
+
 	else
 	{
 		last = *head;
@@ -38,5 +42,6 @@ list_t *add_node_end(list_t **head, const char *str)
 			last = last->next;
 		last->next = new;
 	}
+
 	return (*head);
 }
